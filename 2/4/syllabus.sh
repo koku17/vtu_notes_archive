d2715f06904dd5ec2d587dfaeb3be0fa8289eb4e
hackdev17 <youremail@yourdomain.com>
2024-04-22 18:19:15 +0530

    Add BFE syllabus

---
+++ 2/4/syllabus.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -nc --no-check-certificate https://vtu.ac.in/pdf/2022common3to8/BBOK407.pdf
+wget -nc --no-check-certificate https://vtu.ac.in/pdf/2022common3to8/BUHK408.pdf
+wget -nc --no-check-certificate https://vtu.ac.in/pdf/2022common3to8/BNSK359.pdf
+wget -nc --no-check-certificate https://vtu.ac.in/pdf/2022_3to8/2aimlsyll.pdf
